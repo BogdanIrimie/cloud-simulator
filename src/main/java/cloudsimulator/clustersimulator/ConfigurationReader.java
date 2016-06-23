100e5d55d3be753f9597506bab43e59b0d6a3fa9
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-06-23 15:45:42 +0200

    Extract cluster data from JSON. Add test JSON.

---
+++ src/main/java/cloudsimulator/clustersimulator/ConfigurationReader.java
@@ -0,0 +1,35 @@
+package cloudsimulator.clustersimulator;
+
+
+import cloudsimulator.clustersimulator.dto.SimulatedCluster;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Read cluster configuration from file.
+ */
+public class ConfigurationReader {
+
+    public static void main(String[] args) {
+        ConfigurationReader configurationReader = new ConfigurationReader();
+        configurationReader.readClusterConfiguration();
+    }
+
+    public SimulatedCluster readClusterConfiguration() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        String path = classLoader.getResource("clusterConfiguration.json").getFile();
+
+        ObjectMapper mapper = new ObjectMapper();
+        SimulatedCluster simulatedCluster = new SimulatedCluster();
+
+        try {
+            simulatedCluster = mapper.readValue(new File(path), SimulatedCluster.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return simulatedCluster;
+    }
+}
