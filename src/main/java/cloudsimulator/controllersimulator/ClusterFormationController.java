3acd0a439085b0d6033985abbb0dc846e87bed98
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-07-01 14:48:39 +0200

    Add dummy logic in ClusterFormationController, just to increment the number of VMs.

--- src/main/java/cloudsimulator/controllersimulator/ClusterFormationController.java
+++ src/main/java/cloudsimulator/controllersimulator/ClusterFormationController.java
@@ -1,4 +1,17 @@
 package cloudsimulator.controllersimulator;
 
+import cloudsimulator.clustersimulator.ClusterManager;
+
 public class ClusterFormationController {
+
+
+    public void allocateVMs(long numberOfVmToAllocate, ClusterManager clusterManager) {
+        clusterManager.getCluster().getTgGroup().stream()
+                .findFirst()
+                .ifPresent(tgGroup -> {
+                    tgGroup.setVmNumber(tgGroup.getVmNumber() + numberOfVmToAllocate);
+                });
+    }
+
+
 }
