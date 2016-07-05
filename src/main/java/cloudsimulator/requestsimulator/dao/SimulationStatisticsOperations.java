6b6ce7d0ffb0215443dc07955c0705282bc0f941
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-07-05 11:06:29 +0200

    Add comment for dao.

--- src/main/java/cloudsimulator/requestsimulator/dao/SimulationStatisticsOperations.java
+++ src/main/java/cloudsimulator/requestsimulator/dao/SimulationStatisticsOperations.java
@@ -3,5 +3,8 @@ package cloudsimulator.requestsimulator.dao;
 import cloudsimulator.requestsimulator.dto.SimulationStatistics;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/**
+ * Define operations on SimulationStatistics
+ */
 public interface SimulationStatisticsOperations extends MongoRepository<SimulationStatistics, String> {
 }
