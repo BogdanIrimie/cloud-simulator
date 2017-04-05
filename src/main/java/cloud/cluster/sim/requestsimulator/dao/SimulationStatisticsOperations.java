55a6b663997390c58675f5f7e02ebeeb62601128
Bogdan Irimie <bogdan.irimie@amorphsys.com>
2017-04-05 20:30:15 +0300

    Add component annotation in order to be able to auto wire component.

--- src/main/java/cloud/cluster/sim/requestsimulator/dao/SimulationStatisticsOperations.java
+++ src/main/java/cloud/cluster/sim/requestsimulator/dao/SimulationStatisticsOperations.java
@@ -2,9 +2,11 @@ package cloud.cluster.sim.requestsimulator.dao;
 
 import cloud.cluster.sim.requestsimulator.dto.SimulationStatistics;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 
 /**
  * Define operations on SimulationStatistics
  */
+@Component
 public interface SimulationStatisticsOperations extends MongoRepository<SimulationStatistics, String> {
 }
